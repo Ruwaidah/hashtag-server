ef7cb0649e3040350575e0d5acdd1f371075d72f
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2024-01-25 21:20:00 -0600

    connection socket io with client

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 import dotenv from "dotenv";
+import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 dotenv.config();
-const httpServer = createServer();
-const io = new Server(httpServer, {
+
+const app = express();
+const server = createServer(app);
+
+const PORT = process.env.PORT;
+
+const io = new Server(server, {
   cors: {
-    origin:
-      process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"],
+    origin: "http://localhost:3000",
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(`user ${socket.id} connected`);
-  socket.on("message", (data) => {
-    console.log(data);
-    io.emit("message", `${socket.id.substring(0.5)} :  ${data}`);
-  });
+  console.log(socket.id);
 });
 
-httpServer.listen(3500, () => {
-  console.log("listening on port 3500");
+app.get("/", (req, res) => {
+  res.status(200).json("WE LIVE");
 });
+server.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+// const httpServer = createServer();
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin:
+//       process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`user ${socket.id} connected`);
+//   socket.on("message", (data) => {
+//     console.log(data);
+//     io.emit("message", `${socket.id.substring(0.5)} :  ${data}`);
+//   });
+// });
+
+// httpServer.listen(4000, () => {
+//   console.log("listening on port 4000");
+// });
